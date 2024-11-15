04c21b03024bdf5f10f4ac9314e2728da986b16b
Alok_Sharma <aloksharma132005@gmail.com>
2024-11-15 20:11:48 +0530

    first API_CALLS practice

---
+++ API_TESTING.py
@@ -0,0 +1,22 @@
+import requests
+
+def api_calls():
+    base_url = 'https://api.freeapi.app/api/v1/public/randomproducts/product/random'
+    response = requests.get(url=base_url)
+    data = response.json()
+    # print(data)
+
+    if data.get("success") and "data" in data:
+        product_data = data["data"]
+        print(f"Id : {product_data['id']}\nTitle : {product_data['title']}\nDiscription : {product_data['description']}\nSuccess : {data['success']}")
+    else:
+        raise Exception("Failed to fetch the data!")
+
+def main():
+    try:
+        api_calls()
+    except Exception as e:
+        print(str(e))
+
+if __name__ == "__main__":
+    main()
--- main.py
+++
@@ -1,2 +0,0 @@
-print("Hello World")
-# so now what i have to make is a video recommendation algorithm 
