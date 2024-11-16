c8cc5550815f91b363a834eaee582b194fd13ca3
Alok_Sharma <aloksharma132005@gmail.com>
2024-11-16 20:11:35 +0530

    API_test.py

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import requests
+
+def api_calls():
+    base_url = 'https://jsonplaceholder.typicode.com/posts/1'  # Update post with ID 1
+    payload = {'title': 'Updated Title','body': 'This is the updated body content', 'userId': 1}
+    
+    response = requests.put(url=base_url, json=payload)
+    
+    if response.status_code == 200:
+        data = response.json()
+        print("Updated Data:", data)
+    else:
+        print("Failed to update the resource. Status Code:", response.status_code)
+
+def main():
+    try:
+        api_calls()
+    except Exception as e:
+        print(str(e))
+
+if __name__ == "__main__":
+    main()
