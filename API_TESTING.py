1966d249cf27b7a4c0ebec35f6522d657d44d5f0
Alok_Sharma <aloksharma132005@gmail.com>
2024-11-16 20:10:19 +0530

    GET_API_REQUEST

--- API_TESTING.py
+++ API_TESTING.py
@@ -1,20 +1,16 @@
 import requests
 
 def api_calls():
-    base_url = 'https://api.freeapi.app/api/v1/kitchen-sink/http-methods/put'
-    payload = {'connection': 'none'}  # Example payload, you may adjust it as needed
-    response = requests.put(url=base_url, json=payload)  # Use PUT instead of GET
-    try:
-        data = response.json()  # Attempt to parse the response as JSON
-        print(data)
+    base_url = 'https://api.freeapi.app/api/v1/kitchen-sink/http-methods/get'
+    response = requests.get(url=base_url)
+    data = response.json()
+    # print(data)
 
-        if data.get("success") and "data" in data:
-            product_data = data["data"]
-            print(f"Method : {product_data['method']}\nSomething : {product_data['headers']['connection']}")
-        else:
-            raise Exception("Failed to fetch the data!")
-    except ValueError:
-        print("Error: Response is not in valid JSON format.")
+    if data.get("success") and "data" in data:
+        product_data = data["data"]
+        print(f"Method : {product_data['method']}\nConnection : {product_data['headers']['connection']}")
+    else:
+        raise Exception("Failed to fetch the data!")
 
 def main():
     try:
